8cf4214561fdd97efab625c72d43dc88440a3d67
O <105786517+oparvizi@users.noreply.github.com>
2023-01-19 00:43:26 +0100

    Update ComplexSurveys.R

--- ComplexSurveys.R
+++ ComplexSurveys.R
@@ -2,5 +2,8 @@
 title: "Complex Surveys"
 output: html_document
 date: "2023-01-15"
-source: 
+source: https://www.wiley.com/en-us/Complex+Surveys%3A+A+Guide+to+Analysis+Using+R-p-9780470284308
 ---
+
+# The functions readexport 0, read.dta0, and read.spss0 will read SAS XPORT, Stata, and SPSS files.
+library(foreign) 
